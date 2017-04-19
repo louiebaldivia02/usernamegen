e1673d3e4e001d3a70a044781ebb6f7604f5f03a
louiebaldivia02 <louie.baldivia01@gmail.com>
2017-04-19 22:10:06 +0800

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+  Welcome: <?php echo $_POST["name"]; ?><br>
+  Your Company Name is: <?php echo $_POST["company"]; ?><br>
+  
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+include ("helloform.html");
+?>
