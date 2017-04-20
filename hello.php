11ec5c1abb263d6fc6d0c556d503e0df5c81cba0
louiebaldivia02 <louie.baldivia01@gmail.com>
2017-04-20 09:06:52 +0800

    Updated stylesheet
    Added Stylesheet

--- hello.php
+++ hello.php
@@ -1,8 +1,7 @@
 <html>
 <body>
-
+	<link rel="stylesheet" type="text/css" href="style.css">
   Welcome: <?php echo $_POST["name"]; ?><br>
   Your Company Name is: <?php echo $_POST["company"]; ?><br>
-  
 </body>
 </html>
